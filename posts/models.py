59d86fd14f939cc4a85ef6e01364b2693817b978
Myronov Maksym <mm@rollncode.com>
2020-04-12 22:58:33 +0300

    api

--- posts/models.py
+++ posts/models.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-
     title = models.CharField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.created)
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    like = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created)
--- social_network/urls.py
+++ social_network/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_jwt.views import refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',
          include([
-             # path(r'auth/', include('djoser.urls')),
-             # path('api-auth/', include('rest_framework.urls')),
              re_path(r'^rest-auth/', include('rest_auth.urls')),
              re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+             re_path(r'^refresh-token/', refresh_jwt_token),
+             path('', include('posts.urls')),
          ])),
 ]
